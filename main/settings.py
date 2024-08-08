d152758f47e46d51871214cc9eecd28cfbffea6f
OclooEmmanuel <oclooemmanuel02@gmail.com>
2024-08-08 15:35:07 +0100

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = os.path.join(BASE_DIR,'static_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 
 # Default primary key field type
