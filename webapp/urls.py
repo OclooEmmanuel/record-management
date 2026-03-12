cf3d9c336b2a0926763c714299bb504469b09f6c
ocloo <oclooemmanuel02@gmail.com>
2026-03-12 17:27:03 +0000

    CRUD endpoints added to api.py

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,7 +5,7 @@ from .api import *
 
 
 urlpatterns = [
-    path('', views.home, name=''),
+    path('', views.home, name='home'),
     path('register', views.register, name='register' ),
     path('my-login', views.my_login, name='my-login'),
     path('user-logout',views.user_logout, name='user-logout'),
@@ -17,10 +17,19 @@ urlpatterns = [
     path('view-record/<int:pk>',views.view_record, name='view-record'),
     path('delete-record/<int:pk>', views.delete_record, name='delete-record'),
 
-    # ---API
+
+    # ---API(S)
+    # AUTH
     path('api/register', api_register, name='api-register'),
     path('api/login', api_login, name='api-register'),
     path('api/logout', api_logout, name='api-register'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+
+    # CRUD ENDPOINTS
+    path('api/records', api_get_records, name='api-get-records'),
+    path('api/create', api_create_record, name='api-create-record'),
+    path('api/record/<int:pk>', api_get_record, name='api-get-record'),
+    path('api/update/<int:pk>', api_update_record, name='api-update-record'),
+    path('api/delete/<int:pk>', api_delete_record, name='api-delete-record'),
 ]
 
